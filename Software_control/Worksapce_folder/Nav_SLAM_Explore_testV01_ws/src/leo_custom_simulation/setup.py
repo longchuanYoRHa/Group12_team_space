032aa42bc5bf3744c58bf996dab5c02b46a90a6a
longchuanYoRHa <2674312287@qq.com>
2026-02-10 21:54:21 +0000

    adding camera to simulation

--- Software_control/Worksapce_folder/Nav_SLAM_Explore_testV01_ws/src/leo_custom_simulation/setup.py
+++ Software_control/Worksapce_folder/Nav_SLAM_Explore_testV01_ws/src/leo_custom_simulation/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'config'), glob('config/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
